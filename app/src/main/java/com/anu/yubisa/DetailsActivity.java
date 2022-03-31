8b1d6f9f692af39d9a934d4a5586b0c0f4c97a48
Anggra <anggrapmb@gmail.com>
2022-03-31 16:29:09 +0700

    memperbaiki bug harga

--- app/src/main/java/com/anu/yubisa/DetailsActivity.java
+++ app/src/main/java/com/anu/yubisa/DetailsActivity.java
@@ -10,7 +10,8 @@ import android.widget.TextView;
 public class DetailsActivity extends AppCompatActivity {
     ImageButton kurang, tambah;
     TextView jumlah, harga;
-    int count, total;
+    int count, total, price2;
+    String price;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +21,15 @@ public class DetailsActivity extends AppCompatActivity {
         tambah = findViewById(R.id.btn_11);
         jumlah = findViewById(R.id.textView13);
         harga = findViewById(R.id.textView14);
-        String price = harga.getText().toString();
-        int price2 = Integer.parseInt(price);
+        price = harga.getText().toString();
+        price2 = Integer.parseInt(price);
 
         tambah.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view){
                 count++;
                 total = price2 * count;
-                jumlah.setText(Integer.toString(count));
-                harga.setText(Integer.toString(total));
+                jumlah.setText(count + "");
+                harga.setText(total + "");
             }
         });
 
