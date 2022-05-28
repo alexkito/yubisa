d06943bcebe44daa8b3533154bb2ff2ba77156e9
LJ <LJ>
2022-05-28 12:32:13 +0700

    final bagian main

--- app/src/main/java/com/anu/yubisa/MainActivity.java
+++ app/src/main/java/com/anu/yubisa/MainActivity.java
@@ -2,6 +2,7 @@ package com.anu.yubisa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,9 +16,11 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    DatabaseHelper db;
+
     EditText edtNomor;
-    ImageView btnKonversi;
-    TextView txtyen, txturo, txtusd, txtwon, txtruble;
+    ImageView btnKonversi, logout;
+    TextView txtyen, txturo, txtusd, txtwon;
     double angka;
 
     @Override
@@ -25,12 +28,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        db = new DatabaseHelper(this);
+
         edtNomor = findViewById(R.id.edtNomor);
         btnKonversi = findViewById(R.id.btnKonversi);
         txtyen = findViewById(R.id.txtyen);
         txturo = findViewById(R.id.txturo);
         txtusd = findViewById(R.id.txtusd);
         txtwon = findViewById(R.id.txtwon);
+        logout = (ImageView) findViewById(R.id.btn_logout);
+
+        Boolean checkSession = db.checkSession("ada");
+        if (checkSession == false) {
+            Intent loginIntent = new Intent(MainActivity.this, Login.class);
+            startActivity(loginIntent);
+            finish();
+        }
+
+        // logout
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Boolean updtSession = db.upgradeSession("kosong", 1);
+                if (updtSession == true) {
+                    Toast.makeText(getApplicationContext(), "Berhasil Keluar", Toast.LENGTH_SHORT).show();
+                    Intent loginIntent = new Intent(MainActivity.this, Login.class);
+                    startActivity(loginIntent);
+                    finish();
+                }
+            }
+        });
+
 
         btnKonversi.setOnClickListener(new View.OnClickListener() {
             @Override
